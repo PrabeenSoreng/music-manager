7d525b5b40e2be8faeb93c3dd2df283b52b681a9
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-07-15 22:19:57 +0530

    manager page created

---
+++ musicapp/pages/manager/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <manager />
+  </div>
+</template>
+
+<script>
+import manager from "@/components/manager/manager";
+export default {
+  components: {
+    manager
+  }
+};
+</script>
