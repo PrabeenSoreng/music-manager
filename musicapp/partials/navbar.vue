ad83f68f7612d95d12dad04bf42e3d5d13da2605
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-07-15 21:26:41 +0530

    updating the layout

---
+++ musicapp/partials/navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+      <div class="container">
+        <a href="#" class="navbar-brand">Music App</a>
+        <button
+          class="bavbar-toggler"
+          type="button"
+          data-toggler="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a href="#" class="nav-link">Player</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Manager</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.nav-link,
+.navbar-brand {
+  color: #ffff !important;
+}
+</style>
